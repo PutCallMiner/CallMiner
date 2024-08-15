595e15eb1d612586f56f212a459755551790904a
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-08-15 16:13:00 +0200

    Chore: pass mongo port and host and env vars; create client & db globally

--- BEServer/be/config.py
+++ BEServer/be/config.py
@@ -3,3 +3,5 @@ import os
 MONGO_ROOT_USERNAME = os.environ["MONGO_ROOT_USERNAME"]
 MONGO_ROOT_PASSWORD = os.environ["MONGO_ROOT_PASSWORD"]
 MONGO_DB_NAME = os.environ["MONGO_DB_NAME"]
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = os.environ["MONGO_PORT"]
