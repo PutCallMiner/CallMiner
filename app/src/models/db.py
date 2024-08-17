8aa02309cc009d9fd8737794d843cff2e51c9c85
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-08-17 12:09:08 +0200

    fix: update .env.example, use given a port

--- app/src/models/db.py
+++ app/src/models/db.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio as motor
 import os
 
+MONGO_HOST = os.environ["MONGO_HOST"]
 MONGO_ROOT_USERNAME = os.environ["MONGO_ROOT_USERNAME"]
 MONGO_ROOT_PASSWORD = os.environ["MONGO_ROOT_PASSWORD"]
 MONGO_DB_NAME = os.environ["MONGO_DB_NAME"]
@@ -9,7 +10,7 @@ MONGO_PORT = os.environ["MONGO_PORT"]
 
 async def get_db():
     client = motor.AsyncIOMotorClient(
-        f"mongodb://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@mongo:{MONGO_PORT}",
+        f"mongodb://{MONGO_ROOT_USERNAME}:{MONGO_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}",
         connect=True,
     )
     db = client[MONGO_DB_NAME]
--- app/src/tasks/__init__.py
+++ app/src/tasks/__init__.py
@@ -1,10 +1,12 @@
 import celery
 import os
 
+REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
 
 app = celery.Celery(
     "callminer",
-    broker=f"redis://redis:{REDIS_PORT}/0",
-    backend=f"redis://redis:{REDIS_PORT}/0",
+    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    include=["tasks"],
 )
