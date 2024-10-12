c8f1b4f875ba9bb339988db504e66f1729f1f0be
Wojciech Mączka <wojciech.maczka@protonmail.com>
2024-10-12 20:59:53 +0200

    Update recordings.py
    remove the leftover debug statement

--- WebServer/webapp/routers/recordings.py
+++ WebServer/webapp/routers/recordings.py
@@ -19,7 +19,6 @@ async def table(
     skip: int = 0,
     take: int = 20,
 ) -> HTMLResponse:
-    print("search:", search)
     query = {"recording_url": {"$regex": search, "$options": "i"}} if search else None
     total = await count_recordings(db, query=query)
     recordings = await get_recordings(db, query=query, skip=skip, take=take)
