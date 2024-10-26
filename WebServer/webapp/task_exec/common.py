743f5a9d560a76e2d0913a391f8b5d50770f4c15
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-10-26 18:08:14 +0000

    Refactor: move schedule logic from DAG to Scheduler

--- WebServer/webapp/task_exec/common.py
+++ WebServer/webapp/task_exec/common.py
@@ -1,5 +1,6 @@
 from enum import StrEnum, auto
 from functools import cache
+from typing import TypeAlias
 
 from webapp.task_exec.tasks import (
     ASRTask,
@@ -8,7 +9,7 @@ from webapp.task_exec.tasks import (
     SpeakerClassifyTask,
     SummarizeTask,
 )
-from webapp.task_exec.utils import DAG, GraphDict, Schedule
+from webapp.task_exec.utils import DAG
 
 
 class TaskType(StrEnum):
@@ -31,18 +32,34 @@ def get_task_by_type(task_type: TaskType):
     return component_to_task[task_type]
 
 
+Schedule: TypeAlias = list[list[TaskType]]
+
+
 class Scheduler:
-    dep_graph: GraphDict[TaskType] = {
-        TaskType.ASR: set(),
-        TaskType.NER: {TaskType.ASR},
-        TaskType.SPEAKER_CLASS: {TaskType.ASR},
-        TaskType.SUMMARY: {TaskType.ASR, TaskType.SPEAKER_CLASS},
-        TaskType.CONFORMITY: {TaskType.ASR, TaskType.SPEAKER_CLASS},
-    }
-    dep_dag = DAG(dep_graph)
+    dep_dag = DAG(
+        {
+            TaskType.ASR: set(),
+            TaskType.NER: {TaskType.ASR},
+            TaskType.SPEAKER_CLASS: {TaskType.ASR},
+            TaskType.SUMMARY: {TaskType.ASR, TaskType.SPEAKER_CLASS},
+            TaskType.CONFORMITY: {TaskType.ASR, TaskType.SPEAKER_CLASS},
+        }
+    )
 
     @classmethod
-    def get_execution_schedule(
-        cls, target_task_types: set[TaskType]
-    ) -> Schedule[TaskType]:
-        return cls.dep_dag.get_schedule(target_task_types)
+    def get_execution_schedule(cls, target_task_types: set[TaskType]) -> Schedule:
+        schedule: Schedule = []
+
+        all_deps = cls.dep_dag.get_all_descendants(target_task_types)
+        for task_type in cls.dep_dag.topological_order:
+            if task_type not in all_deps:
+                continue
+            if (
+                len(schedule) > 0
+                and len(cls.dep_dag.get_outnodes(task_type).intersection(schedule[-1]))
+                == 0
+            ):
+                schedule[-1].append(task_type)
+            else:
+                schedule.append([task_type])
+        return schedule
--- WebServer/webapp/task_exec/utils.py
+++ WebServer/webapp/task_exec/utils.py
@@ -26,7 +26,6 @@ async def async_request_with_timeout(
 
 T = TypeVar("T", bound=Hashable)
 GraphDict: TypeAlias = dict[T, set[T]]
-Schedule: TypeAlias = list[list[T]]
 
 
 class DAG(Generic[T]):
@@ -45,7 +44,10 @@ class DAG(Generic[T]):
     def topological_order(self):
         return list(TopologicalSorter(self._graph_data).static_order())
 
-    def _get_all_descendants(self, target_nodes: set[T]) -> set[T]:
+    def get_outnodes(self, node: T) -> set[T]:
+        return self._graph_data[node]
+
+    def get_all_descendants(self, target_nodes: set[T]) -> set[T]:
         all_deps = set()
         queue = list(target_nodes)
         while len(queue) > 0:
@@ -54,19 +56,3 @@ class DAG(Generic[T]):
             all_deps.update(self._graph_data[task_t])
             queue.extend(self._graph_data[task_t] - all_deps)
         return all_deps
-
-    def get_schedule(self, target_nodes: set[T]) -> Schedule:
-        schedule: Schedule = []
-
-        all_deps = self._get_all_descendants(target_nodes)
-        for task_type in self.topological_order:
-            if task_type not in all_deps:
-                continue
-            if (
-                len(schedule) > 0
-                and len(self._graph_data[task_type].intersection(schedule[-1])) == 0
-            ):
-                schedule[-1].append(task_type)
-            else:
-                schedule.append([task_type])
-        return schedule
