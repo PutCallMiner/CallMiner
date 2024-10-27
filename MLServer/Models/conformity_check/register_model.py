fe2f5a8de48bc6c0ee8088d80704e03d9bf4e36e
KarlsonAV <andreykartavik@gmail.com>
2024-10-27 20:08:02 +0000

    feature(conformity_check): Conformity check model in docker-compose

---
+++ MLServer/Models/conformity_check/register_model.py
@@ -0,0 +1,16 @@
+import mlflow
+import openai
+from model import MAX_TOKENS, MESSAGES, MODEL, TEMPERATURE
+
+if __name__ == "__main__":
+    with mlflow.start_run():
+        mlflow.openai.log_model(
+            model=MODEL,
+            registered_model_name="conformity_check",
+            task=openai.chat.completions,
+            artifact_path="model",
+            messages=MESSAGES,
+            pip_requirements="requirements.txt",
+            temperature=TEMPERATURE,
+            max_tokens=MAX_TOKENS,
+        )
