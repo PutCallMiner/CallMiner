955c5cdb7d398b37fd3420b11930fc678f579c10
BbqGamer <korba.adam@gmail.com>
2024-11-24 21:33:52 +0100

    feat: Follow the recording and highlight current entry

--- WebServer/public/js/script.js
+++ WebServer/public/js/script.js
@@ -4,3 +4,49 @@ function skipToTime(timeInSeconds) {
   audioPlayer.currentTime = timeInSeconds;
   audioPlayer.play()
 }
+
+let transcriptEntries = Array.from(
+  document.getElementsByClassName("transcript-entry")
+);
+
+const entries = transcriptEntries.map((div) => {
+  const id = div.id;
+  const startTimeMs = parseInt(id.replace('entry-', ''));
+  return { div, startTimeMs };
+});
+
+entries.sort((a, b) => a.startTimeMs - b.startTimeMs);
+
+let previousIndex = -1;
+
+function updateHighlight() {
+  const currentTimeMs = audioPlayer.currentTime * 1000;
+  let newIndex = findCurrentIndex(currentTimeMs);
+  if (newIndex !== previousIndex) {
+    if (previousIndex >= 0) {
+      entries[previousIndex].div.classList.remove('highlighted-entry');
+    }
+
+    if (newIndex >= 0) {
+      entries[newIndex].div.classList.add('highlighted-entry');
+      entries[newIndex].div.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+
+    previousIndex = newIndex;
+  }
+}
+
+function findCurrentIndex(currentTimeMs) {
+  let index = -1;
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i].startTimeMs <= currentTimeMs) {
+      index = i;
+    } else {
+      break;
+    }
+  }
+  return index;
+}
+
+audioPlayer.addEventListener('timeupdate', updateHighlight);
+audioPlayer.addEventListener('seeked', updateHighlight);
